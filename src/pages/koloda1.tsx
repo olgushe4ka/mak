53ea0c4dd0f6337f133bf8d4c9d3dbc9a6125912
Olga <olgushe4ka@gmail.com>
2023-10-28 22:25:14 +0400

    Change img

--- src/pages/koloda1.tsx
+++ src/pages/koloda1.tsx
@@ -4,17 +4,20 @@ import styles from "./pages-styles.module.css";
 import { useState } from "react";
 
 function Koloda1() {
-  const randomIndex = Math.floor(Math.random() * cards1.length);
-
-  const [currentCard, setCurrentCard] = useState(cards1[randomIndex]);
+  const currentCardIndex = Math.floor(Math.random() * cards1.length);
+  const [currentCard, setCurrentCard] = useState(cards1[currentCardIndex]);
 
   const handleImageChange = () => {
-    const randomIndex = Math.floor(Math.random() * cards1.length);
+    let randomIndex;
+    do {
+      randomIndex = Math.floor(Math.random() * cards1.length);
+    } while (randomIndex === currentCardIndex);
+
     setCurrentCard(cards1[randomIndex]);
   };
 
   return (
-    <div >
+    <div>
       <ImageDescriptionComponent
         photo={currentCard.photo}
         description={currentCard.description}
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -6,7 +6,7 @@ function MainPage() {
       <div className={`${styles.main}`}>
         <div className={styles.imageContainer}>
           <img
-            src={"https://s120vlx.storage.yandex.net/rdisk/bed3c6012d13dcbc4b1ae4a57d37954faf9c53784c7445ecbaeedee787722045/653d6aac/83T559gpby_WQZIEdFh8bq5vnTqMwkhxcSySME0eD6xFosJNQU59sO7s1BusaZmphUmpbukMgpHuF373z2PxwQ==?uid=704432166&filename=makcard.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=704432166&fsize=22156&hid=356e8915f3b03b7249d63b6829e2866b&media_type=image&tknv=v2&etag=dd1a13c8b08312f7512e240c0c037097&rtoken=3ydI352D4Col&force_default=yes&ycrid=na-0bcce13669dea29c34aed17d57e1dc0a-downloader4f&ts=608cc64ef0300&s=e374d5b1cbb51d8023c4aca7e321a2ce9326c6eeb9fc61add5e5b7d45ec6cf66&pb=U2FsdGVkX1-apTQ_0eceJ6N7qkUFFiC4enXqyo-4_LbMPvdjssThYcIRDwKAA5DjECRKP9RSGwG3UaRq2ex7jVyfXjkbPHqUNsKJ_2GsDT0"}
+            src={require("../images/makcard.jpg")}
             alt="logo"
           />{" "}
         </div>
